80211f30217cbea97647fc50513471600e86d513
Ethan Chen <intervigil@gmail.com>
2015-12-23 19:38:28 -0800

    hiaeul: Add initial files

---
+++ cm.mk
@@ -0,0 +1,18 @@
+$(call inherit-product, device/htc/hiaeul/full_hiaeul.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+PRODUCT_NAME := cm_hiaeul
+BOARD_VENDOR := htc
+TARGET_VENDOR := htc
+PRODUCT_DEVICE := hieul
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="htc_hiaeul" \
+    PRODUCT_NAME="hiaeul_00617" \
+    BUILD_FINGERPRINT="htc/hiaeul_00617/htc_hiaeul:6.0/MRA58K/639737.6:user/release-keys" \
+    PRIVATE_BUILD_DESC="1.12.617.6 CL639737 release-keys"
