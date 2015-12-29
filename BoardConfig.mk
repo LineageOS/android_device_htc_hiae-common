85265b62762b7ddd77542adb0f296b1e88744e0a
Ethan Chen <intervigil@gmail.com>
2015-12-29 15:51:14 -0800

    hiaeul: Allow installation on htc_hiaeul devices

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,8 @@ DEVICE_PATH := device/htc/hiaeul
 
 TARGET_SPECIFIC_HEADER_PATH := device/htc/hiaeul/include
 
+TARGET_OTA_ASSERT_DEVICE := htc_hiaeul,hiaeul
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
