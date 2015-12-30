6f8eb74862555bb27dc147d0073c142a3480b51c
Ethan Chen <intervigil@gmail.com>
2015-12-29 18:28:13 -0800

    hiaeul: Match config to sound card type
    * Device has sound card considered external, match expected name

--- device.mk
+++ device.mk
@@ -79,9 +79,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/configs/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/configs/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/mixer_paths_wcd9330.xml:system/etc/mixer_paths_wcd9330.xml
 
 # Display
 PRODUCT_PACKAGES += \
