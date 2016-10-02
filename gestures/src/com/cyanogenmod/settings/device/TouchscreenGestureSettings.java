e41c5e265a6c2510e42e77474ac522f6f9e7cfe7
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-01 23:52:34 -0700

    hiae: remove back button from action bar
    Starting N, top-level Settings activities should a drawer menu up
    on the action bar. Remove the up/back button as it is now obsolete.
    
    Change-Id: Iad035bc484f37329e67ed0247ff947849ec689c7

--- gestures/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
+++ gestures/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -16,13 +16,11 @@
 
 package com.cyanogenmod.settings.device;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
-import android.view.MenuItem;
 
 public class TouchscreenGestureSettings extends PreferenceActivity {
 
@@ -38,9 +36,6 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
         mDoubleTapPreference = (SwitchPreference) findPreference(KEY_DOUBLE_TAP_ENABLE);
         mDoubleTapPreference.setChecked(isDoubleTapEnabled());
         mDoubleTapPreference.setOnPreferenceChangeListener(mDoubleTapPrefListener);
-
-        final ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -48,15 +43,6 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
         super.onResume();
 
         getListView().setPadding(0, 0, 0, 0);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        return false;
     }
 
     private boolean enableDoubleTap(boolean enable) {
