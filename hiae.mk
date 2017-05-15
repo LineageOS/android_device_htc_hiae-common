eb74e1c949b581c6da077abdb935a7ce1631bbbf
Harry Youd <harry@harryyoud.co.uk>
2017-05-15 08:25:23 +0100

    Remove Gello from PRODUCT_PACKAGES
    Change-Id: I320f68285ad826fbdb3ad75a335c701656c595d6

--- hiae.mk
+++ hiae.mk
@@ -91,10 +91,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml
 
-# Browser
-PRODUCT_PACKAGES += \
-    Gello
-
 # Camera
 PRODUCT_PACKAGES += \
     libshim_camera \
