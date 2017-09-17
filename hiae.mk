c64cc2afebc3f31ed49515191014601217b7e17e
Ethan Chen <intervigil@gmail.com>
2017-09-16 23:19:53 -0700

    hiae: Add vibrator HAL in place of CMHW implementation
    Change-Id: I95d502df9c06e258c519f32d793cafc4a67db6b7

--- hiae.mk
+++ hiae.mk
@@ -301,7 +301,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.hiae
 
 # Wifi
 PRODUCT_PACKAGES += \
