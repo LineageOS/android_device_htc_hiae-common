86a6882ec35a605294a89f97387c2c9f2785dffa
Ethan Chen <intervigil@gmail.com>
2017-09-17 02:27:36 -0700

    hiae: Convert lights HAL to native binderized HAL
    Change-Id: I43673573af05d1118cbcd3d0146749f143745a84

--- hiae.mk
+++ hiae.mk
@@ -195,9 +195,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8952
+    android.hardware.light@2.0-service.hiae
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
