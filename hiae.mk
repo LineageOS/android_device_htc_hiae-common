5acf97fd4661068ed680f610f3c1231180ebfa09
Ethan Chen <intervigil@gmail.com>
2017-09-21 00:08:18 -0700

    hiae: Add xtwifi.conf
    * Original package version: 2.18.617.30
    
    Change-Id: Iab35fb6995347d3e3db8878a6ee9b63199ab1564

--- hiae.mk
+++ hiae.mk
@@ -122,7 +122,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/configs/xtwifi.conf:system/etc/xtwifi.conf
 
 # HTC Logging functions
 PRODUCT_PACKAGES += \
