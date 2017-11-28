3999d4403c55e840c279d0851675849e52296651
Ethan Chen <intervigil@gmail.com>
2017-11-27 20:49:11 -0800

    Revert "hiae: Use Lineage legacy camera provider"
    This reverts commit e384ccf2a8caf89157ff0b6c259488152ecefbc2.
    
    Change-Id: I16da7cbe24b627d3ce78f316d8be469337c2a277

--- hiae.mk
+++ hiae.mk
@@ -101,7 +101,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     libshim_camera \
     Snap
 
