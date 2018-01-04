672494910166fefded6632ce12ebb3f216b753a8
Ethan Chen <intervigil@gmail.com>
2018-01-03 22:57:56 -0800

    DO NOT MERGE: hiae: SELinux permissive
    Change-Id: I1749a926657e96bba66a49cc908123901108a047

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,6 +45,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x00078000
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01f88000 --tags_offset 0x01d88000
